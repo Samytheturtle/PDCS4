0d0a435c05564e21117a8d5c59b4efa075467cf6
samyt <samyt@DESKTOP-L76978L>
2021-06-14 18:29:28 -0500

    Cambio 1

--- JavaFXApplicationPDS4/src/javafxapplicationpds4/vistas/FXMLLoginController.java
+++ JavaFXApplicationPDS4/src/javafxapplicationpds4/vistas/FXMLLoginController.java
@@ -5,14 +5,20 @@
  */
 package javafxapplicationpds4.vistas;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -25,8 +31,6 @@ public class FXMLLoginController implements Initializable {
     private PasswordField tfPassword;
     @FXML
     private TextField tfUsuario;
-    @FXML
-    private Label label;
     @FXML
     private Label lbErrorUsuario;
     @FXML
@@ -42,6 +46,34 @@ public class FXMLLoginController implements Initializable {
 
     @FXML
     private void clicBtnIniciar(ActionEvent event) {
+        lbErrorPassword.setText("");
+        lbErrorUsuario.setText("");
+      String  txUsuario =tfUsuario.getText();
+      String  txPassword=tfPassword.getText();
+    
+      boolean isCorrecto = true;
+      if(txUsuario.isEmpty()){
+        lbErrorUsuario.setText("Campo requerido");
+        isCorrecto = false;
+      }
+   
+      if(txPassword.isEmpty()){
+        lbErrorPassword.setText("Campo requerido");
+        isCorrecto = false;
+      }  
+      if(isCorrecto){
+          PantallaPrincipal();
+      }
+    }
+    private void PantallaPrincipal(){
+        try {
+            Stage stage = (Stage) tfUsuario.getScene().getWindow();
+            Scene scenePrincipal = new Scene(FXMLLoader.load(getClass().getResource("FXMLPrincipal.fxml")));
+            stage.setScene(scenePrincipal);
+            stage.show();
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
     
 }
