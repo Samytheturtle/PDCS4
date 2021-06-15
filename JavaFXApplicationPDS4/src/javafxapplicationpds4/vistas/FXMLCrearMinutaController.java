03616725224fdb91ce3c30e0767273e916201bbe
kevin <kevin@K99>
2021-06-15 10:25:58 -0500

    hay vamoooos

--- JavaFXApplicationPDS4/src/javafxapplicationpds4/vistas/FXMLCrearMinutaController.java
+++ JavaFXApplicationPDS4/src/javafxapplicationpds4/vistas/FXMLCrearMinutaController.java
@@ -5,18 +5,27 @@
  */
 package javafxapplicationpds4.vistas;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonBar;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 import modelo.DAO.IntegranteDAO;
 import modelo.DAO.ReunionDAO;
 import modelo.pojo.Acuerdo;
@@ -78,7 +87,8 @@ public class FXMLCrearMinutaController implements Initializable {
         cbReunion.setItems(reuniones);
     }
     private void cargaIntegrantes(){
-        integrantes.addAll()
+        integrantes.addAll(IntegranteDAO.getAllIntegrantes());
+        cbResponsable.setItems(integrantes);
     }
 
     @FXML
@@ -111,6 +121,35 @@ public class FXMLCrearMinutaController implements Initializable {
 
     @FXML
     private void clicBtnCancelar(ActionEvent event) {
+        if(confirmarAlerta("Confirmación", "¿Está seguro que desea cancelar la Minuta?").toString() == "OK_DONE"){
+          changeWindow("FXMLPrincipal.fxml", event);
+        }
+    }
+    
+    private ButtonBar.ButtonData confirmarAlerta(String titulo, String mensaje){
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle(titulo);
+        alert.setHeaderText(null);
+        alert.setContentText(mensaje);
+        alert.showAndWait();
+        
+        return alert.getResult().getButtonData();
+    }
+    
+    private FXMLLoader changeWindow(String window, Event event){
+        Stage stage = new Stage();
+        FXMLLoader loader = null;
+        try{
+            loader = new FXMLLoader(getClass().getResource(window));
+            stage.setScene(new Scene((Pane)loader.load()));
+            stage.show(); 
+            Stage currentStage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+            currentStage.close();
+        } catch(IOException io){
+             System.out.println(io.getMessage());
+        } finally {
+            return loader;
+        }
     }
     
 }
