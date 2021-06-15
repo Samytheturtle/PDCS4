1cb6c94fd67327ff5bbbe45fcc0d10a6e96a3d26
kevin <kevin@K99>
2021-06-14 19:09:32 -0500

    cambio codigo

--- JavaFXApplicationPDS4/src/modelo/DAO/ReunionDAO.java
+++ JavaFXApplicationPDS4/src/modelo/DAO/ReunionDAO.java
@@ -5,10 +5,40 @@
  */
 package modelo.DAO;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import modelo.ConexionBD;
+import modelo.pojo.Reunion;
+
 /**
  *
  * @author Lenovo
  */
 public class ReunionDAO {
-    
+    public static ArrayList<Reunion> getAllReuniones(){
+        ArrayList<Reunion> reuniones = new ArrayList<>();
+        Connection conn = ConexionBD.abrirConexionBD();
+        if(conn != null){
+            try{
+                String consulta = "SELECT * FROM reunion";
+                PreparedStatement ps = conn.prepareStatement(consulta);
+                ResultSet resultado = ps.executeQuery();
+                while(resultado.next()){
+                    Reunion reu = new Reunion();
+                    reu.setIdReunion(resultado.getInt("idReunion"));
+                    reu.setAsunto(resultado.getString("asunto"));
+                    System.out.println("Asunto reunion: "+reu.getAsunto());
+                    reuniones.add(reu);
+                }
+                conn.close();
+            }catch(SQLException e){
+                System.out.println("ERROR: "+e.getMessage());
+                e.printStackTrace();
+            }
+        }
+        return reuniones;
+    }
 }
