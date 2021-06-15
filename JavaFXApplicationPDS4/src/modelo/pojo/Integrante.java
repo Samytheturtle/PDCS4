94deaa370cda2f0fad65e61c7771f3109e5259c1
kevin <kevin@K99>
2021-06-15 00:17:21 -0500

    hay vamoooos

--- JavaFXApplicationPDS4/src/modelo/pojo/Integrante.java
+++ JavaFXApplicationPDS4/src/modelo/pojo/Integrante.java
@@ -10,5 +10,105 @@ package modelo.pojo;
  * @author Lenovo
  */
 public class Integrante {
+    private String nombre;
+    private String programaEducativoImpacto;
+    private String telefono;
+    private String perfilPROMPEP;
+    private String nivelSNI;
+    private String iesGradoMaximo;
+    private String gradoMaximoEstudio;
+    private String curp;
+    private String correoElectronico;
+    private String cargo;
+    private String areaPerteneciente;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getProgramaEducativoImpacto() {
+        return programaEducativoImpacto;
+    }
+
+    public void setProgramaEducativoImpacto(String programaEducativoImpacto) {
+        this.programaEducativoImpacto = programaEducativoImpacto;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getPerfilPROMPEP() {
+        return perfilPROMPEP;
+    }
+
+    public void setPerfilPROMPEP(String perfilPROMPEP) {
+        this.perfilPROMPEP = perfilPROMPEP;
+    }
+
+    public String getNivelSNI() {
+        return nivelSNI;
+    }
+
+    public void setNivelSNI(String nivelSNI) {
+        this.nivelSNI = nivelSNI;
+    }
+
+    public String getIesGradoMaximo() {
+        return iesGradoMaximo;
+    }
+
+    public void setIesGradoMaximo(String iesGradoMaximo) {
+        this.iesGradoMaximo = iesGradoMaximo;
+    }
+
+    public String getGradoMaximoEstudio() {
+        return gradoMaximoEstudio;
+    }
+
+    public void setGradoMaximoEstudio(String gradoMaximoEstudio) {
+        this.gradoMaximoEstudio = gradoMaximoEstudio;
+    }
+
+    public String getCurp() {
+        return curp;
+    }
+
+    public void setCurp(String curp) {
+        this.curp = curp;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getAreaPerteneciente() {
+        return areaPerteneciente;
+    }
+
+    public void setAreaPerteneciente(String areaPerteneciente) {
+        this.areaPerteneciente = areaPerteneciente;
+    }
+    
     
 }
