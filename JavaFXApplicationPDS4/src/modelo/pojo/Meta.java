d0f0d31d9d2eafdb44d91d6a441e76bffab50315
samyt <samyt@DESKTOP-L76978L>
2021-06-17 05:26:28 -0500

    Plan de trabajo sin terminar

--- JavaFXApplicationPDS4/src/modelo/pojo/Meta.java
+++ JavaFXApplicationPDS4/src/modelo/pojo/Meta.java
@@ -10,5 +10,53 @@ package modelo.pojo;
  * @author Lenovo
  */
 public class Meta {
+    private String nombre;
+    private String planeacion;
+    private int idPlanTrabajo;
+    private int idMeta;
     
+    public Meta(String objetivo,String planeacion, int idPlanTrabajo, int idMeta) {
+        this.nombre = nombre;
+        this.planeacion = planeacion;
+        this.idPlanTrabajo = idPlanTrabajo;
+        this.idMeta = idMeta;
+   }
+        
+    public Meta(String objetivo,String planeacion) {
+        this.nombre = nombre;
+        this.planeacion = planeacion;
+        this.idPlanTrabajo = 0;
+        this.idMeta = 0;
+   }
+
+    public Meta() {}
+   
+    public String getnombre() {
+        return nombre;
+    }
+
+    public String getPlaneacion() {
+        return planeacion;
+    }
+
+    public int getIdPlanTrabajo() {
+        return idPlanTrabajo;
+    }
+    public int getIdMeta() {
+        return idMeta;
+    }
+    public void setnombre(String nombre) {
+        this.nombre=nombre;
+    }
+
+    public void setPlaneacion(String planeacion) {
+        this.planeacion=planeacion;
+    }
+
+    public void setIdPlanTrabajo(int idplan) {
+        this.idPlanTrabajo=idplan;
+    }
+    public void setIdMeta(int idMeta) {
+        this.idMeta=idMeta;
+    }    
 }
